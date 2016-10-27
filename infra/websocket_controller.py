80ad1fb2d80a8f73a2512ac203ec05d00778b724
tukejonny <tukejonny@gmail.com>
2016-10-28 03:56:49 +0900

    write websocket_controller

--- infra/websocket_controller.py
+++ infra/websocket_controller.py
@@ -9,5 +9,17 @@ simple_switch_instance_name = 'simple_switch_api_app'
 url = '/simpleswitch/ws'
 
 class WebSocketController(ControllerBase):
-    def __init__(self):
-        pass
+    def __init__(self, req, link, data, **config):
+        super(WebSocketController, self).__init__(req, link, data, **config)
+
+        self.simple_switch_app = data[simple_switch_instance_name]
+
+    @websocket('simpleswitch', url)
+    def _websocket_handler(self, ws):
+        simple_switch = self.simple_switch_app
+
+        simple_switch.logger.debug('Websocket connected: {0}'.format(ws))
+        rpc_server = WebSocketRPCServer(ws, simple_switch)
+        rpc_server.serve_forever()
+        simple_switch.logger.debug('Websocket disconnected: {0}'.format(ws))
+

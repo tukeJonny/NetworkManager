181491acfa73c37483e2d5067897e0862cee4525
tukejonny <tukejonny@gmail.com>
2016-12-07 22:25:11 +0900

    add REST API (GET switch info)

--- infra/websocket_controller.py
+++ infra/websocket_controller.py
@@ -1,10 +1,13 @@
 #-*- coding: utf-8 -*-
 from ryu.app.wsgi import ControllerBase
 from ryu.app.wsgi import websocket
-from ryu.app.wsgi import rpc_public, WebSocketRPCServer
+from ryu.app.wsgi import (
+    rpc_public,
+    WebSocketRPCServer
+)
 from ryu.lib import hub
 
-simple_switch_instance_name = 'simple_switch_api_app'
+simple_switch_instance_name = 'network_manager_api_app'
 
 url = '/simpleswitch/ws'
 
